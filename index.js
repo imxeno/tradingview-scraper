aef3801ea380ff8efd7fea0caa4e6d1347006096
imXeno <xeno@xeno.yt>
2018-06-08 19:40:10 +0200

    the project is now a lib, not an express application

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
 'use strict';
 
-var fs = require('fs');
-var express = require('express');
-var TradingView = require('./lib/tradingview');
-
-var app = express();
-var tradingView = new TradingView();
-
-// pair route
-app.get('/api/v1/pair/:pair', function (req, res) {
-    var ticker_name = req.params.pair;
-    
-    tradingView.getTicker(ticker_name, function(api_err, data) {
-        res.json(data);
-    });
-})
-
-// listen
-fs.unlink("api.sock", function() {
-  app.listen("api.sock", function() {
-    fs.chmod('api.sock', 0o777, function() {});
-  });
-});
+module.exports = require('./lib/tradingview');
