e5c68f892459a1e8eeb5e9977aa10e9af0b74e17
Martin Kask <martin@industrial.ninja>
2022-12-19 21:44:31 +0200

    Style fixes

--- examples/bitcoinTickerWithAuth.ts
+++ examples/bitcoinTickerWithAuth.ts
@@ -1,29 +1,21 @@
-const { TradingViewAPI } = require("../dist");
+import { TradingViewAPI } from '../dist';
 
-const bitcoinSymbol = "BTCUSD";
+const bitcoinSymbol = 'BTCUSD';
 const tv = new TradingViewAPI();
 
-const token = "YOUR_AUTHENTICATED_TOKEN";
+const token = 'YOUR_AUTHENTICATED_TOKEN';
 
-process.stdout.write("Loading...");
+console.log('Loading...');
 
 tv.setAuthToken(token);
 
 tv.setup().then(() => {
   tv.getTicker(bitcoinSymbol).then(ticker => {
     let last = 0;
-    ticker.on("update", data => {
+    ticker.on('update', data => {
       if (data.lp && data.lp !== last) {
-        process.stdout.clearLine();
-        process.stdout.cursorTo(0);
-        process.stdout.write(
-          "[" +
-            bitcoinSymbol +
-            "] " +
-            (last > data.lp ? "-" : "+") +
-            " " +
-            data.lp.toFixed(2) +
-            " "
+        console.log(
+          '[' + bitcoinSymbol + '] ' + (last > data.lp ? '-' : '+') + ' ' + data.lp.toFixed(2) + ' '
         );
         last = data.lp;
       }
